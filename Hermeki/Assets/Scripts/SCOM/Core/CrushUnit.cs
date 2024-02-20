3d8c5c8b456506fd2487754a72095ec2b414906d
KDH <ekgns0131@daum.net>
2024-02-21 02:51:31 +0900

    Add CrushUnit 닿는 적에게 공격

---
+++ Hermeki/Assets/Scripts/SCOM/Core/CrushUnit.cs
@@ -0,0 +1,23 @@
+﻿using SCOM.CoreSystem;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CrushUnit : CoreComponent
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //사망 시
+        if (!core.Unit.IsAlive)
+            return;
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Damageable") && collision.tag != this.tag) 
+        {
+            if (collision.TryGetComponent(out IDamageable victim))
+            {
+                victim.Damage(core.Unit, 1, 1);
+            }
+        }
+    }
+}
