26e21e0f8c0a9305a9c5b08c75e3b2ad0d6ceb8d
KDH <ekgns0131@daum.net>
2024-02-21 02:54:57 +0900

    Add EditorEnemySpanwManager 임의 Enemy 생성 스크립트

---
+++ Hermeki/Assets/Scripts/Manager/EditorEnemySpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorEnemySpawnManager : MonoBehaviour
+{
+    public List<GameObject> EnemyList = new List<GameObject>();
+
+    public float interval;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        for (int i = 0; i < EnemyList.Count; i++)
+        {
+            Spawn(transform.position + Vector3.right * (i * interval), EnemyList[i]);
+        }        
+    }
+
+    private void Spawn(Vector3 pos, GameObject enemy)
+    {
+        GameObject _enemy = Instantiate(enemy);
+        _enemy.transform.position = pos;
+    }
+}
