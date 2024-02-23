12ff62bd8c55760305e46daa20992f7919e9c2d2
KDH <ekgns0131@daum.net>
2024-02-23 23:22:30 +0900

    Fix FlyEnemyAI add TargetReachedAction, Unit 스크립트에서 OnTargetReached()시점에 함수 호출하도록 Action 추가

--- Hermeki/Assets/Scripts/AI/FlyEnemyAI/FlyEnemyAI.cs
+++ Hermeki/Assets/Scripts/AI/FlyEnemyAI/FlyEnemyAI.cs
@@ -1,52 +1,21 @@
 using Pathfinding;
 using Pathfinding.Util;
+using System;
 using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEngine;
 
 public class FlyEnemyAI : AIPath
 {
-    [Header("Custom")]
-    public float activateDistance;
-    public bool isFollow = false;
-    protected override void FixedUpdate()
+    private Enemy enemy;
+    public Action TargetReachedAction;
+    protected override void Awake()
     {
-        base.FixedUpdate();        
-        if (velocity2D.x > 0.05f)
-        {
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-        }
-        else if (velocity2D.x < -0.05f)
-        {
-            transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-        }
-        Debug.Log($"velocity2D = {velocity2D}");
+        base.Awake();
+        enemy = this.GetComponent<Enemy>();
     }
-
-    protected override void Update()
-    {
-        base.Update();
-        if (!TargetInDistance())
-        {
-            target = null;
-        }
-    }
-
-    private bool TargetInDistance()
-    {
-        if (target == null)
-            return false;
-        return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
-    }
-
     public override void OnTargetReached()
     {
         base.OnTargetReached();
-        target = null;
+        TargetReachedAction?.Invoke();
     }
-
-    protected override void OnDrawGizmosSelected()
-    {
-        base.OnDrawGizmosSelected();
-        Draw.Gizmos.Cylinder(position, Vector3.forward, 0, activateDistance, UnityEngine.Color.blue);
-    }    
 }
