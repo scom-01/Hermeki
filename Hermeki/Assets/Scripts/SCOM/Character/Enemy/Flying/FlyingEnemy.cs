36f9e6ae312907ac806d3101d32f6846011dcf5b
KDH <ekgns0131@daum.net>
2024-02-23 23:21:29 +0900

    Add FlyEnemy,Flying_Enemy_State 공중 유닛 스크립트 추가

---
+++ Hermeki/Assets/Scripts/SCOM/Character/Enemy/Flying/FlyingEnemy.cs
@@ -0,0 +1,49 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : Enemy
+{
+    [HideInInspector]
+    public FlyEnemyAI EnemyAI;
+
+
+    public virtual Flying_Enemy_AttackState AttackState { get; private set; }
+    public virtual Flying_Enemy_IdleState IdleState { get; private set; }
+    public virtual Flying_Enemy_MoveState MoveState { get; private set; }
+    public virtual Flying_Enemy_HitState HitState { get; private set; }
+    public virtual Flying_Enemy_DeathState DeathState { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //°¢ State »ý¼º
+        IdleState = new Flying_Enemy_IdleState(this, "idle");
+        MoveState = new Flying_Enemy_MoveState(this, "move");
+        AttackState = new Flying_Enemy_AttackState(this, "action");
+        DeathState = new Flying_Enemy_DeathState(this, "death");
+        EnemyAI = this.GetComponent<AIPath>() as FlyEnemyAI;
+        EnemyAI.radius = CC2D.size.x / 2;
+
+        EnemyAI.TargetReachedAction += () => FSM.ChangeState(AttackState);
+    }
+    protected override void Init()
+    {
+        base.Init();
+        FSM.Initialize(IdleState);
+    }
+
+    public override void DieEffect()
+    {
+        base.DieEffect();
+        EnemyAI.enabled = false;
+        FSM.ChangeState(DeathState);
+    }
+    public override void SetTarget(Unit unit)
+    {
+        base.SetTarget(unit);
+        EnemyAI.target = unit?.transform;
+    }
+}
---
+++ Hermeki/Assets/Scripts/SCOM/Character/Enemy/Flying/State/Flying_Enemy_State.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Flying_Enemy_IdleState : EnemyState
+{
+    FlyingEnemy FlyingEnemy;
+    public Flying_Enemy_IdleState(Unit unit, string animBoolName) : base(unit, animBoolName)
+    {
+        FlyingEnemy = unit as FlyingEnemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (Death.isDead)
+            return;
+
+        if (EnemyCollisionSenses.isUnitDetectedCircle)
+        {
+            FlyingEnemy.SetTarget(EnemyCollisionSenses.UnitDetectedCircle?.GetComponent<Unit>());
+        }
+        else
+        {
+            FlyingEnemy.SetTarget(null);
+        }    
+
+        //타겟 방향 회전
+        unit.Core.CoreMovement.FlipToTarget();
+
+        if (enemy.GetTarget() != null)
+        {
+            FlyingEnemy.FSM.ChangeState(FlyingEnemy.MoveState);
+            return;
+        }
+    }
+}
+
+public class Flying_Enemy_AttackState : EnemyState
+{
+    FlyingEnemy FlyingEnemy;
+    public Flying_Enemy_AttackState(Unit unit, string animBoolName) : base(unit, animBoolName)
+    {
+        FlyingEnemy = unit as FlyingEnemy;
+    }
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+        if(isAnimationFinished)
+        {
+            FlyingEnemy.FSM.ChangeState(FlyingEnemy.IdleState);
+        }
+    }
+}
+public class Flying_Enemy_MoveState : EnemyState
+{
+    FlyingEnemy FlyingEnemy;
+    public Flying_Enemy_MoveState(Unit unit, string animBoolName) : base(unit, animBoolName)
+    {
+        FlyingEnemy = unit as FlyingEnemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        FlyingEnemy.EnemyAI.canMove = true;
+    }
+    public override void Exit()
+    {
+        base.Exit();
+        FlyingEnemy.EnemyAI.canMove = false;
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (Death.isDead)
+            return;
+        //타겟 방향 회전
+        unit.Core.CoreMovement.FlipToTarget();
+
+        if (!EnemyCollisionSenses.CheckUnitDetectedCircle(unit.GetTarget()))
+        {
+            FlyingEnemy.SetTarget(null);
+            FlyingEnemy.FSM.ChangeState(FlyingEnemy.IdleState);
+            return;
+        }
+    }
+}
+
+public class Flying_Enemy_HitState : EnemyHitState
+{
+    public Flying_Enemy_HitState(Unit unit, string animBoolName) : base(unit, animBoolName)
+    {
+    }
+
+    public override void IdleState()
+    {
+        
+    }
+}
+
+public class Flying_Enemy_DeathState : EnemyDeathState
+{
+    public Flying_Enemy_DeathState(Unit unit, string animBoolName) : base(unit, animBoolName)
+    {
+    }
+    public override void Enter()
+    {
+        base.Enter();
+        unit.RB.gravityScale = 5;
+        unit.Core.CoreMovement.SetVelocityZero();
+    }
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+}
