1de9d03493f632426308aef73b5e103b8496f990
KDH <ekgns0131@daum.net>
2024-03-05 17:41:07 +0900

    Add ItemSpawnManager 테스트 레벨에서 사용할 아이템 스폰 매니져

---
+++ Hermeki/Assets/Scripts/Manager/EditorItemSpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorItemSpawnManager : MonoBehaviour
+{
+    public List<GameObject> ItemList = new List<GameObject>();
+
+    public float interval;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        for (int i = 0; i < ItemList.Count; i++)
+        {
+            Spawn(transform.position + Vector3.right * (i * interval), ItemList[i]);
+        }        
+    }
+
+    private void Spawn(Vector3 pos, GameObject enemy)
+    {
+        GameObject _enemy = Instantiate(enemy);
+        _enemy.transform.position = pos;
+    }
+}
