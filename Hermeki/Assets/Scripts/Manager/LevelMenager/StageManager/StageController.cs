2578b32315afcb572bffd4c507c9d41540325bac
KDH <ekgns0131@daum.net>
2024-03-13 16:12:19 +0900

    Add StageController 스테이지 컨트롤러 추가

---
+++ Hermeki/Assets/Scripts/Manager/LevelMenager/StageManager/StageController.cs
@@ -0,0 +1,56 @@
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class StageController : MonoBehaviour
+{
+    public Transform StartPos;
+    public StageEndPoint EndPos;
+    private LevelManager LM;
+    public TilemapRenderer TM_Renderer;
+    public PolygonCollider2D PC2D;
+    private void Awake()
+    {
+        LM = GetComponentInParent<LevelManager>();
+        TM_Renderer = GetComponentInChildren<TilemapRenderer>();
+        TM_Renderer.enabled = false;
+    }
+    public bool EnterEndpos(Player player)
+    {
+        if (player == null)
+            return false;
+        if (LM.GoNextStage(player))
+        {
+            ControllTM(false);
+        }
+        return true;
+    }
+
+    public bool StartStage()
+    {
+        if (!ControllTM(true))
+        {
+            return false;
+        }
+        
+        LM.VirtualCamera.GetComponent<CinemachineConfiner2D>().enabled = true;
+        if (PC2D != null)
+        {
+            LM.VirtualCamera.GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = PC2D;
+        }
+
+        if (this.TryGetComponent(out ActionEventHandler _action))
+        {
+            _action.Action();
+        }
+        return true;
+    }
+    private bool ControllTM(bool isTrue)
+    {
+        if (TM_Renderer == null)
+            return false;
+
+        TM_Renderer.enabled = isTrue;
+        return true;
+    }
+}
