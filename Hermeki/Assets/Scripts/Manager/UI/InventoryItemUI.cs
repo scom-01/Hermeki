eca1938c233e52a3e232b714b303c4a563fd8e61
KDH <ekgns0131@daum.net>
2024-03-21 22:21:59 +0900

    Fix InventoryItemUI 아이템 타입에 맞게 컨텐츠 표시 수정

--- Hermeki/Assets/Scripts/Manager/UI/InventoryItemUI.cs
+++ Hermeki/Assets/Scripts/Manager/UI/InventoryItemUI.cs
@@ -1,24 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.UI;
+
+[Serializable]
+public struct InventoryContent
+{
+    public GameObject ContentParent;
+    public Image Item_Img;
+    public TMP_Text Durability_Txt;
+    public TMP_Text Equip_Txt;
+}
 
 public class InventoryItemUI : MonoBehaviour
 {
     [SerializeField]
     private EquipItemData EquipData;
-    [SerializeField]
-    private Image Item_Img;
-    [SerializeField]
-    private TMP_Text Durability_Txt;
-    private UI_Canvas _ParentCanvas;
+
     private InventoryTable _ParentInventoryTable;
-    private EquipItemData _currEquipItemData;
-    private void Start()
+
+    public List<InventoryContent> inventoryContents = new List<InventoryContent>();
+    private void Awake()
     {
         _ParentInventoryTable = this.GetComponentInParent<InventoryTable>();
-        _ParentCanvas = this.GetComponentInParent<UI_Canvas>();
     }
 
     /// <summary>
@@ -32,8 +37,26 @@ public class InventoryItemUI : MonoBehaviour
             return false;
 
         EquipData = _data;
-        SetSpriteUI(EquipData);
-        SetDurabilityTxt(EquipData);
+        SetActiveContent();
+        return true;
+    }
+
+    private bool SetActiveContent()
+    {
+        if (inventoryContents?.Count == 0)
+            return false;
+
+        foreach (var Content in inventoryContents)
+        {
+            Content.ContentParent.SetActive(false);
+        }
+        if (inventoryContents.Count == (int)Item_Type.Count)
+        {
+            inventoryContents[(int)EquipData.dataSO.ItemType].ContentParent.SetActive(true);
+            SetSpriteUI(EquipData, inventoryContents[(int)EquipData.dataSO.ItemType].Item_Img);
+            SetDurabilityTxt(EquipData, inventoryContents[(int)EquipData.dataSO.ItemType].Durability_Txt, inventoryContents[(int)EquipData.dataSO.ItemType].Equip_Txt);
+        }
+
         return true;
     }
 
@@ -42,21 +65,70 @@ public class InventoryItemUI : MonoBehaviour
     /// </summary>
     /// <param name="_data"></param>
     /// <returns></returns>
-    private bool SetSpriteUI(EquipItemData _data)
+    private bool SetSpriteUI(EquipItemData _data, Image _img)
     {
-        if (Item_Img == null || _data?.dataSO == null)
+        if (_img == null || _data?.dataSO == null)
             return false;
 
-        Item_Img.sprite = _data.CalculateSprite()[0];
+        _img.sprite = _data.CalculateSprite()[0];
 
         return true;
     }
 
-    private bool SetDurabilityTxt(EquipItemData _data)
+    private bool SetDurabilityTxt(EquipItemData _data, TMP_Text _durability_Txt, TMP_Text _equip_Txt)
     {
-        if (Durability_Txt == null || _data?.dataSO == null)
+        if (_data?.dataSO == null)
             return false;
-        Durability_Txt.text = String.Format($"{_data.CurrentDurability}/{_data.dataSO.MaxDurability}");
+
+        if (_durability_Txt != null)
+            _durability_Txt.text = String.Format($"{_data.CurrentDurability}/{_data.dataSO.MaxDurability}");
+
+        if (checkEquip(_data))
+        {
+            if (_equip_Txt != null)
+                _equip_Txt.text = String.Format($"E");
+        }
+        else
+        {
+            if (_equip_Txt != null)
+                _equip_Txt.text = String.Format($"");
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// true = 장착 중
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    private bool checkEquip(EquipItemData data)
+    {
+        if (data?.dataSO == null)
+        {
+            return false;
+        }
+        switch (data.dataSO.ItemType)
+        {
+            case Item_Type.Armor:
+                if (_ParentInventoryTable?.unit?.ItemManager?.CheckEquipArmor(data) == null)
+                {
+                    return false;
+                }
+                break;
+            case Item_Type.Weapon:
+                if (_ParentInventoryTable?.unit?.ItemManager?.CheckEquipWeapon(data) == null)
+                {
+                    return false;
+                }
+                break;
+            case Item_Type.Rune:
+                return false;
+            case Item_Type.Count:
+                return false;
+            default:
+                return false;
+        }
+
         return true;
     }
 
@@ -74,14 +146,13 @@ public class InventoryItemUI : MonoBehaviour
             switch (EquipData.dataSO.ItemType)
             {
                 case Item_Type.Armor:
-                    _ParentInventoryTable.unit?.ItemManager?.ChangeArmor(EquipData);
-                    _ParentInventoryTable.SetState(InventoryState.Close);
+                    _ParentInventoryTable.SetState(InventoryState.ChoiceEquip);
                     break;
                 case Item_Type.Weapon:
-                    _ParentInventoryTable.SetState(InventoryState.ChoiceHand);
+                    _ParentInventoryTable.SetState(InventoryState.ChoiceEquip);
                     return;
                 case Item_Type.Rune:
-                    _ParentInventoryTable.SetState(InventoryState.Enchant);
+                    _ParentInventoryTable.SetState(InventoryState.ChoiceEnchant);
                     return;
                 default:
                     break;
@@ -95,6 +166,7 @@ public class InventoryItemUI : MonoBehaviour
             {
                 case Item_Type.Armor:
                     break;
+
                 case Item_Type.Weapon:
                     break;
                 case Item_Type.Rune:
@@ -120,7 +192,7 @@ public class InventoryItemUI : MonoBehaviour
                                     temp = new EquipItemData(runeData.enchantMethods[i].Result, runeData.enchantMethods[i].Result.MaxDurability);
 
                                     _ParentInventoryTable.unit?.ItemManager?.EnchantArmor(temp);
-                                    _ParentInventoryTable.CurrItem.DecreaseDurability();                                    
+                                    _ParentInventoryTable.CurrItem.DecreaseDurability();
                                     break;
                                 }
                             }
@@ -132,15 +204,13 @@ public class InventoryItemUI : MonoBehaviour
                                 if (runeData.enchantMethods[i].ItemStyle == (AllItemStyle)(EquipData.dataSO as WeaponItemDataSO).Style)
                                 {
                                     //인챈트된 새로운 아이템
-                                    temp = new EquipItemData(runeData.enchantMethods[i].Result, runeData.enchantMethods[i].Result.MaxDurability);                                    
-                                    //_ParentInventoryTable.unit?.ItemManager?.Remove_InventoryWeaponItem(EquipData);
+                                    temp = new EquipItemData(runeData.enchantMethods[i].Result, runeData.enchantMethods[i].Result.MaxDurability);
                                     if (ResearchItem(EquipData) != -1)
                                     {
 
                                     }
                                     _ParentInventoryTable.unit.ItemManager.ResearchItem(EquipData).SetEquipItemData(temp);
-                                    //_ParentInventoryTable.unit.ItemManager.EnchantWeapon(temp);// SetState(InventoryState.ChoiceHand);
-                                    _ParentInventoryTable.CurrItem.DecreaseDurability();                                    
+                                    _ParentInventoryTable.CurrItem.DecreaseDurability();
                                     break;
                                 }
                             }
