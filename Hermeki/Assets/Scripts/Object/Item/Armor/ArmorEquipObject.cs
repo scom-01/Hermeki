53400a4169b3df898bca028ec9ea19052af29cd0
KDH <ekgns0131@daum.net>
2024-03-22 23:39:45 +0900

    Fix ArmorEquipObject 방어구 타입에 맞게 SpriteRender를 교체

--- Hermeki/Assets/Scripts/Object/Item/Armor/ArmorEquipObject.cs
+++ Hermeki/Assets/Scripts/Object/Item/Armor/ArmorEquipObject.cs
@@ -1,16 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ArmorEquipObject : EquipObject
 {
     public Unit targetUnit;
     public Rect rect;
-
+    public ArmorStyle armorStyle;
+    public List<Transform> SpriteTransformList = new List<Transform>();
+    public SpriteRenderer[] SpriteRenderers;
     public ArmorEquipObject(EquipItemData data) : base(data)
     {
         Type = Item_Type.Armor;
+        
     }
 
-    public SpriteRenderer[] SpriteRenderers => GetComponentsInChildren<SpriteRenderer>();
     public override void Interactive(Unit unit)
     {
         Debug.Log($"Armor Equip Interactive {unit.name}");
@@ -22,9 +25,23 @@ public class ArmorEquipObject : EquipObject
         Debug.Log($"Armor Equip UnInteractive {unit.name}");
     }
 
+    public override bool SetData(EquipItemData _data, bool _isEquipable = false)
+    {
+        if (_data?.dataSO == null)
+            return false;
+        armorStyle = (_data.dataSO as ArmorItemDataSO).Style;
+        return base.SetData(_data, _isEquipable);
+    }
     [ContextMenu("SetSpriteRenderer")]
     public override void SetSpriteRenderer()
     {
+        foreach (var _transform in SpriteTransformList)
+        {
+            _transform.gameObject.SetActive(false);
+        }
+        SpriteTransformList[(int)armorStyle].gameObject.SetActive(true);
+        SpriteRenderers = SpriteTransformList[(int)armorStyle].gameObject.GetComponentsInChildren<SpriteRenderer>();
+
         if (SpriteRenderers == null || Data.dataSO == null || Data.CurrentDurability == 0)
         {
             this.GetComponentInParent<Rigidbody2D>().gameObject.SetActive(false);
@@ -42,7 +59,8 @@ public class ArmorEquipObject : EquipObject
 
         for (int i = 0; i < SpriteRenderers.Length; i++)
         {
-            SpriteRenderers[i].sprite = Data.CalculateSprite()[i];
+            if (Data.CalculateSprite() != null && i < Data.CalculateSprite().Length)
+                SpriteRenderers[i].sprite = Data.CalculateSprite()[i];
         }
     }
 }
