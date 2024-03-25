5bd8cbfb58e7bd4cd346ace97edeb3f0c44280c4
KDH <ekgns0131@daum.net>
2024-03-25 14:51:19 +0900

    Fix SpawnItemEvent 유닛 사망 시 아이템 및 오브젝트 스폰 수정

--- Hermeki/Assets/Scripts/Events/Unit/SpawnItemEvent.cs
+++ Hermeki/Assets/Scripts/Events/Unit/SpawnItemEvent.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class SpawnItemEvent : UnitEvent
 {
     public List<EquipItemData> equipItems;
+    public List<AssetReferenceGameObject> UnitAssets;
     public override void Action()
     {
         if (unit == null)
@@ -13,6 +15,11 @@ public class SpawnItemEvent : UnitEvent
         foreach (var item in equipItems)
         {
             GameManager.Inst.LevelManager.CurrStage()?.SO_Controller?.SpawnItem(item, unit.Core.CoreCollisionSenses.UnitCenterPos);
+        }
+
+        foreach (var item in UnitAssets)
+        {
+            GameManager.Inst.LevelManager.CurrStage()?.SO_Controller?.SpawnUnit(item, unit.Core.CoreCollisionSenses.UnitCenterPos);
         }
     }
 }
