24e2d6e798cbb9ddfa1fc098c2fcdc5490e12d01
KDH <ekgns0131@daum.net>
2024-03-31 21:44:35 +0900

    Fix MovingPlatform 기존- Start, End 두개의 포인트의 위치로만 이동, 오직 반복뿐 수정 - 여러개의 포인트로 오각형, 삼각형 같은 움직임 가능, 방향을 수정할 수 있도록 수정 U자, ㄴ자 같은 형태의 움직임 가능

--- Hermeki/Assets/Scripts/SCOM/Interactive/MovingPlatform.cs
+++ Hermeki/Assets/Scripts/SCOM/Interactive/MovingPlatform.cs
@@ -1,50 +1,97 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 
 public class MovingPlatform : MonoBehaviour
 {
+    /// <summary>
+    /// 이동할 Platform
+    /// </summary>
     public Transform platform;
-    public Transform StartPoint;
-    public Transform EndPoint;
-
+    /// <summary>
+    /// 플랫폼이 이동할 경로의 포인트, direction에 따라 이동
+    /// </summary>
+    public List<Transform> Points = new List<Transform>();
+    /// <summary>
+    /// 현재 목적지 포인트 인덱스
+    /// </summary>
+    private int currPointIdx;
+    /// <summary>
+    /// 다음 목적지 포인트 방향
+    /// </summary>
     int direction = 1;
+    [SerializeField]
+    /// <summary>
+    /// true : currPointIdx가 Points.Count에 다다른다면 Points[0].position으로 이동
+    /// </summary>
+    bool isRepeat;
+    /// <summary>
+    /// 진행 속도
+    /// </summary>
     [SerializeField] float speed = 1.5f;
 
     private void FixedUpdate()
     {
-        if (platform == null || StartPoint == null || EndPoint == null)
+        if (platform == null || Points?.Count == 0) 
             return;
 
+        //목적지 위치 계산
         Vector2 target = currentMovementTarget();
 
         platform.transform.position = Vector2.MoveTowards(platform.position, target, speed * Time.deltaTime);
 
+        //다음 목적지와의 거리
         float dist = Vector2.Distance(target, platform.position);
+
+        //다음 목적지에 도달했을 때
         if (dist <= 0.1f)
         {
-            direction *= -1;
+            currPointIdx += direction;
+
+            //Points.Count에 도달했을 때
+            if (isRepeat && currPointIdx >= Points.Count)
+            {
+                currPointIdx = 0;
+            }
+            else if (!isRepeat && currPointIdx >= Points.Count)
+            {
+                direction *= -1;
+                currPointIdx = Points.Count - 2;
+            }
+            //Points[0]에 도달했을 때
+            else if (isRepeat && currPointIdx < 0)
+            {
+                currPointIdx = Points.Count - 1;
+            }
+            else if (!isRepeat && currPointIdx < 0)
+            {
+                direction *= -1;
+                currPointIdx = 1;
+            }
         }
     }
 
 
-    Vector2 currentMovementTarget()
-    {
-        if (direction == 1)
-        {
-            return StartPoint.position;
-        }
-        else
-        {
-            return EndPoint.position;
-        }
-    }
-
+    /// <summary>
+    /// 현재 목적지 position
+    /// </summary>
+    /// <returns></returns>
+    Vector2 currentMovementTarget() => Points[currPointIdx].position;
     private void OnDrawGizmos()
     {
-        if (platform != null && StartPoint != null && EndPoint != null)
+        if (platform != null && Points != null)
         {
-            Gizmos.DrawLine(platform.transform.position, StartPoint.position);
-            Gizmos.DrawLine(platform.transform.position, EndPoint.position);
+            for (int i = 0; i < Points.Count; i++)
+            {
+                if (i + 1 < Points.Count)
+                {
+                    Gizmos.DrawLine(Points[i].position, Points[i + 1].position);
+                }
+                else
+                {
+                    Gizmos.DrawLine(Points[i].position, Points[0].position);
+                }
+            }
         }
     }
 }
