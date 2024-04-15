40014bfcf13206ea321852ef98bc83f59d94cd0a
KDH <ekgns0131@daum.net>
2024-04-15 21:02:23 +0900

    Fix ShadowController Raycast에 두 개 이상 hit될 때 발생하던 오류 수정

--- Hermeki/Assets/Scripts/Character/ShadowController.cs
+++ Hermeki/Assets/Scripts/Character/ShadowController.cs
@@ -3,11 +3,17 @@
 public class ShadowController : MonoBehaviour
 {
     Unit unit;
+    /// <summary>
+    /// 그림자 오브젝트 Transform
+    /// </summary>
     [SerializeField]
     Transform ShadowSprite;
     Vector3 ShadowScale;
     SpriteRenderer SR;
     float ShadowAlpha = 1f;
+    /// <summary>
+    /// 그림자 감지 거리
+    /// </summary>
     [SerializeField]
     private float ShadowDistance = 5f;
     protected ContactFilter2D contactFilter_Ground;
@@ -28,28 +34,32 @@ public class ShadowController : MonoBehaviour
     {
         if (unit == null || ShadowSprite == null)
             return;
-        
+
+        //GroundCenterPos에서 Vector2.down 방향으로 ShadowDistance만큼 Raycast
         var count = Physics2D.Raycast(unit.Core.CoreCollisionSenses.GroundCenterPos, Vector2.down, contactFilter_Ground, hitBuffer, ShadowDistance);
         if (count > 0)
         {
             foreach (var hit in hitBuffer)
             {
-                if (hit.rigidbody == null)
-                    continue;
-
-                if (hit.transform.CompareTag("Platform"))
+                if (hit.rigidbody == null || hit.transform.CompareTag("Platform"))
                     continue;
 
                 //hit의 기울기(양수면 hit의 y가 더 낮은 위치, 즉 GroundCenterPos가 hit.point보다 위에 있으면 양수)
                 if (hit.normal.y < 0.9f)
                     continue;
-
+                
+                //raycast hit와의 거리가 그림자 감지 거리보다 작을 때
                 if (hit.distance <= ShadowDistance)
                 {
                     ShadowSprite.transform.position = new Vector3(ShadowSprite.transform.position.x, hit.point.y, 0);
                     ShadowSprite.localScale = ShadowScale * ((ShadowDistance - hit.distance) / ShadowDistance);
                     SR.color = new Color(SR.color.r, SR.color.g, SR.color.b, (ShadowAlpha * ((ShadowDistance - hit.distance) / ShadowDistance)));
                 }
+                else
+                {                    
+                    SR.color = new Color(SR.color.r, SR.color.g, SR.color.b, 0);
+                }
+                break;
             }
         }
         else
