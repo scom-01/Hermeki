544a59d6b22f9ac0ca51809457d227daf6812460
KDH <ekgns0131@daum.net>
2024-05-10 22:16:18 +0900

    Fix SpawnItemEvent 레벨 매니저를 찾지 못할 때의 NullCheck 오류 수정

--- Hermeki/Assets/Scripts/Events/Unit/SpawnItemEvent.cs
+++ Hermeki/Assets/Scripts/Events/Unit/SpawnItemEvent.cs
@@ -1,11 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class SpawnItemEvent : UnitEvent
 {
+    /// <summary>
+    /// 드랍 아이템
+    /// </summary>
     public List<EquipItemData> equipItems;
+    /// <summary>
+    /// 스폰 유닛
+    /// </summary>
     public List<AssetReferenceGameObject> UnitAssets;
     public override void Action()
     {
@@ -14,12 +20,24 @@ public class SpawnItemEvent : UnitEvent
 
         foreach (var item in equipItems)
         {
-            GameManager.Inst.LevelManager.CurrStage()?.SO_Controller?.SpawnItem(item, unit.Core.CoreCollisionSenses.UnitCenterPos);
+            if (GameManager.Inst?.LevelManager?.CurrStage()?.SO_Controller?.SpawnItem(item, unit.Core.CoreCollisionSenses.UnitCenterPos) == null)
+            {
+                //Instantiate(item, unit.Core.CoreCollisionSenses.UnitCenterPos, Quaternion.identity);
+            }
         }
 
         foreach (var item in UnitAssets)
         {
-            GameManager.Inst.LevelManager.CurrStage()?.SO_Controller?.SpawnUnit(item, unit.Core.CoreCollisionSenses.UnitCenterPos);
+            if (GameManager.Inst?.LevelManager?.CurrStage()?.SO_Controller?.SpawnUnit(item, unit.Core.CoreCollisionSenses.UnitCenterPos) == null)
+            {
+                var pos = unit.Core.CoreCollisionSenses.UnitCenterPos;
+                item.InstantiateAsync(this.transform.parent).Completed +=
+                    (AsyncOperationHandle<GameObject> _obj) =>
+                    {
+                        _obj.Result.transform.position = pos;
+                        _obj.Result.AddComponent<SelfCleanup>();
+                    };
+            }
         }
     }
 }
