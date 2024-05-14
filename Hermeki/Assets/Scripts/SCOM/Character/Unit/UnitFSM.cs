668267edc217d9a4c93574769c8cd21e46d58c8a
KDH <ekgns0131@daum.net>
2024-05-14 21:01:04 +0900

    Fix UnitFSM add CompareState, CompareRootState

--- Hermeki/Assets/Scripts/SCOM/Character/Unit/UnitFSM.cs
+++ Hermeki/Assets/Scripts/SCOM/Character/Unit/UnitFSM.cs
@@ -2,7 +2,10 @@ public class UnitFSM
 {
     public UnitState CurrentState { get; private set; }
 
-    //State 변경 후 호출되는 함수
+    /// <summary>
+    /// 초기 State 설정 함수
+    /// </summary>
+    /// <param name="startingState"></param>
     public void Initialize(UnitState startingState)
     {
         //현재 State 적용
@@ -10,7 +13,10 @@ public class UnitFSM
         CurrentState.Enter();
     }
 
-    //State 변경 시 호출되는 함수
+    /// <summary>
+    /// State 변경 시 호출되는 함수
+    /// </summary>
+    /// <param name="newState"></param>
     public void ChangeState(UnitState newState)
     {
         //if (CurrentState == newState) 
@@ -22,4 +28,18 @@ public class UnitFSM
         CurrentState = newState;
         CurrentState.Enter();
     }
+
+    /// <summary>
+    /// CurrentState가 T와 타입이 같은 지 확인, 상속받는 관계여도 true 반환
+    /// </summary>
+    /// <typeparam name="T">제약조건 : UnitState</typeparam>
+    /// <returns></returns>
+    public bool CompareRootState<T>() => CurrentState is T || CurrentState.GetType().IsSubclassOf(typeof(T));
+
+    /// <summary>
+    /// CurrentState가 T와 타입이 같은 지 확인, 상속받는 관계일 땐 false 반환
+    /// </summary>
+    /// <typeparam name="T">제약조건 : UnitState</typeparam>
+    /// <returns></returns>
+    public bool CompareState<T>() where T : UnitState => CurrentState is T;
 }
